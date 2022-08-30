783b4a71134d9ef03373ffacfa9aab0b5eb6799e
Andrew Simonson <asimonson1125@gmail.com>
2022-08-29 23:22:23 -0400

    Get date/time info at submission

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 # Allow all domain names to cross domain
@@ -8,7 +9,14 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/submit', methods=['POST'])
 def submission():
+    time = datetime.now()
+    weekday = time.weekday()
+    date = time.date()
+    hour = time.hour
+    minute = time.minute
+    timeinfo = {'weekday': weekday, 'date': date, 'hour': hour, 'minute': minute}
     data = request.get_json()
+    print("Data from {weekday}, {date} at {hour}:{minute}".format(**timeinfo))
     print("Lower Fitness Center occupant count: " + data['lower'])
     print("Upper Fitness Center occupant count: " + data['upper'])
     print("Aquatics Center occupant count: " + data['aquatics'])
