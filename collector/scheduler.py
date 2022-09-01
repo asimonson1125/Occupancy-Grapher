0f8847e9f50bb892df9a56f0e9ada71d0ae116e7
Andrew Simonson <asimonson1125@gmail.com>
2022-09-01 11:02:51 -0400

    Update scheduler init

--- collector/scheduler.py
+++ collector/scheduler.py
@@ -1,13 +1,15 @@
-import schedule 
+import schedule
 from reqs import getFacilityOccupancy, getStartEndHour
 from threading import Thread
 import time
 from datetime import datetime, timedelta
 from pytz import timezone
 
+
 def getOffset():
     tz = timezone('US/Eastern')
     return datetime.now().hour - datetime.now(tz).hour
+
 
 def format24Time(hour, minute):
     fHour = hour % 24
@@ -19,29 +21,31 @@ def format24Time(hour, minute):
     fMinute = str(minute)
     if minute < 10:
         fMinute = "0" + str(minute)
-    
+
     return(f"{fHour}:{fMinute}")
 
+
 def getSchedule():
+    schedule.clear()
+    schedule.every().day.at("04:30").do(getSchedule)
     start, end = getStartEndHour()
     now = datetime.now()
     offset = getOffset()
+    offset = 4
     if now.hour - offset < 0:
         return
     for i in range(end - start):
         if now.hour <= i+start:
             for x in range(12):
-                if not(now.hour == i+start and now.minute >= x*5):
-                    t = format24Time(i+start-offset, x*5)
+                if not(now.hour - offset == i+start and now.minute >= x*5):
+                    t = format24Time((i+start+offset) % 24, x*5)
                     schedule.every().day.at(t).do(getFacilityOccupancy)
-    endoff = end-offset
-    if endoff >= 0:
-        schedule.every().day.at(str(endoff) + ":00").do(getFacilityOccupancy)
-    
+    endoff = (end+offset) % 24
+    if endoff < 10:
+        endoff = "0" + str(endoff)
+        endoff = str(endoff)
+    schedule.every().day.at(endoff + ":00").do(getFacilityOccupancy)    
 
-def startScheduler():
-    schedule.every().day.at("04:30").do(getSchedule)
-    Thread(target=runSchedule).start()
 
 def runSchedule():
     while True:
@@ -50,6 +54,8 @@ def runSchedule():
         schedule.run_pending()
         time.sleep(10)
 
+
 if __name__ == '__main__':
+    # Starting the server after midnight before facility closing (which may happen if server offset >1) leads to ignoring the rest of that day
     getSchedule()
-    startScheduler()
+    Thread(target=runSchedule).start()
