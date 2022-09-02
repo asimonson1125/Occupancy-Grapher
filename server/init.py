4f8cbdcf7499d067d7bebabc0c490ad9535e915f
Andrew Simonson <asimonson1125@gmail.com>
2022-09-02 09:43:59 -0400

    update strptime format

--- server/init.py
+++ server/init.py
@@ -53,8 +53,8 @@ class Occupancy(db.Model):
         return self.time
     
     def __lt__(self,other):
-        one = datetime.strptime(self.time, '%Y-%m-%d %H:%M:%S %Z%z')
-        two = datetime.strptime(other.time, '%Y-%m-%d %H:%M:%S %Z%z')
+        one = datetime.strptime(self.time, '%Y-%m-%d %H:%M:%S.%f%z')
+        two = datetime.strptime(other.time, '%Y-%m-%d %H:%M:%S.%f%z')
         return(one < two)
             
 
