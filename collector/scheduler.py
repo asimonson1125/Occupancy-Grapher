879157a3edc314ed4d2288204a8c398610cd04cd
Andrew Simonson <asimonson1125@gmail.com>
2022-09-01 21:49:14 -0400

    (re)add scheduler offset

--- collector/scheduler.py
+++ collector/scheduler.py
@@ -23,26 +23,29 @@ def format24Time(hour, minute):
 def getOffset():
     return datetime.now().hour - datetime.now(est).hour
 
-
 def getSchedule():
     schedule.clear()
     negOffset = str((24 - getOffset()) % 24)
+    if int(negOffset) < 10:
+        negOffset = "0" + negOffset
     schedule.every().day.at(negOffset + ":00").do(getSchedule)
     start, end = getStartEndHour()
-    now = datetime.now(est)
-    if now.hour < 0:
+    now = datetime.now()
+    offset = getOffset()
+    offset = 4
+    if now.hour - offset < 0:
         return
     for i in range(end - start):
-        if now.hour <= i+start:
+        if now.hour-offset <= i+start:
             for x in range(12):
-                if not(now.hour == i+start and now.minute >= x*5):
-                    t = format24Time((i+start) % 24, x*5)
+                if not(now.hour - offset == i+start and now.minute >= x*5):
+                    t = format24Time((i+start+offset) % 24, x*5)
                     schedule.every().day.at(t).do(getFacilityOccupancy)
-    end = end - getOffset()
-    if end < 10:
-        end = "0" + str(end)
-    end = str(end)
-    schedule.every().day.at(end + ":00").do(getFacilityOccupancy)    
+    endoff = (end+offset) % 24
+    if endoff < 10:
+        endoff = "0" + str(endoff)
+    endoff = str(endoff)
+    schedule.every().day.at(endoff + ":00").do(getFacilityOccupancy)    
 
 
 def runSchedule():
